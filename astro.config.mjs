e548a1bf255b2b58490ea107591d02d9546611a9
ProfessorGarcia <cagarc12@uiwtx.edu>
2025-10-13 11:26:03 -0500

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://carlosgarcia.works',
   integrations: [
     tailwind({
       applyBaseStyles: false,
